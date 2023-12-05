00b0690f8fda526e1cdcb23caa0dd47fb96cd046
육진혁 (Ivan) <ivan@daangn.com>
2023-12-05 10:45:10 +0900

    temp

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "FullOfOrange",
+  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
+  author: "FullOfOrange",
+  siteUrl: "https://fulloforange.github.io/",
+  links: {
+    github: "https://github.com/fulloforange",
+    linkedIn: "https://linkedin.com",
+    email: "mailto:devhudi@gmail.com",
+    resume: "https://hudi.blog",
+    link: "https://hudi.blog",
+  },
+  utterances: {
+    repo: "fulloforange/fulloforange.github.io",
+    type: "pathname",
+  },
+}
